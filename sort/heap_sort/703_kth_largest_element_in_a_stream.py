691c155c946c29df501fc41596a070cdcbe954d9
angiebao <anqi.bao.tamu@gmail.com>
2021-03-12 01:12:55 -0800

    kth largest with Priority Queue

--- sort/heap_sort/703_kth_largest_element_in_a_stream.py
+++ sort/heap_sort/703_kth_largest_element_in_a_stream.py
@@ -92,3 +92,27 @@
 #        }
 #    }
 # }
+
+from queue import PriorityQueue
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.pq = PriorityQueue()
+        self.k = k
+        for num in nums:
+            self.pq.put(num)
+            if self.pq.qsize() > self.k:
+                self.pq.get()
+
+    def add(self, val: int) -> int:
+        self.pq.put(val)     
+        if self.pq.qsize() > self.k:
+            self.pq.get()
+
+        return self.pq.queue[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
